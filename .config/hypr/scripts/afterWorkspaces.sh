564643a1f9a1c166ba669058633ef51c513822c1
peit_penbottom <luerike12@gmail.com>
2024-04-18 21:40:39 +0200

    no more dots

--- .config/hypr/scripts/afterWorkspaces.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-for (( i = $(hyprctl activeworkspace | head -n 1 | awk -F'[^0-9]+' '{ print $2 }') + 1 ; i < $( hyprctl workspaces | grep "workspace ID" | sort -r |head -n 1| awk -F'[^0-9]+' '{ print $2 }' ) + 1 | i < 5 ; i++ ))
-do
-	echo -n "$i "
-done
