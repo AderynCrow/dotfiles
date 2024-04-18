bb5ac59c06e227efe498fd3ea833d5a2abceb58d
peit_penbottom <luerike12@gmail.com>
2024-04-18 20:40:44 +0200

    moved files

--- hypr/scripts/prevWorkspaces.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-	for (( i = 1 ; i < $(hyprctl activeworkspace | head -n 1 | awk -F'[^0-9]+' '{ print $2 }') ; i++ ))
-	do
-		echo -n "$i "
-	done
-
--- hypr/scripts/workspacewatcher.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-currentwokspace=$( hyprctl activeworkspace | head -n 1 )
-while true
-do
-	if [[ $currentwokspace != $( hyprctl activeworkspace | head -n 1 ) ]]
-	then
-		exit 0
-	fi
-done
