d7454d3064deae3970e9524ccbd70050d8e544f6
AderynCrow <luerike12@gmail.com>
2024-06-14 20:57:48 +0200

    always record active monitor

--- dot-config/waybar/scripts/toggle-screen-recorder.sh
+++ dot-config/waybar/scripts/toggle-screen-recorder.sh
@@ -6,7 +6,7 @@ status=$?
 if [ $status != 0 ] 
 then 
 	videoName=$(xdg-user-dir VIDEOS)/$(date +'recording_%Y-%m-%d-%H%M%S.mp4')
-	wf-recorder -f $videoName
+	wf-recorder -f $videoName -o $(hyprctl activeworkspace | head -n 1 | awk -F " " '{printf $7}'| rev | cut -c 2- | rev)
 	notify-send "Video saved at $videoName"
 else 
 	pkill --signal SIGINT wf-recorder
